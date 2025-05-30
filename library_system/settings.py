129dbc3dc6916847c902ba88510065dc56bbb3ff
swallahfx <adeniyisaheed75@gmail.com>
2025-05-30 12:19:45 +0100

    change list to str in settings.py

--- library_system/settings.py
+++ library_system/settings.py
@@ -126,7 +126,7 @@ DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'admin@library.com')
 
 REST_FRAMEWORK = {
 
-    'DEFAULT_PAGINATION_CLASS' : ['rest_framework.pagination.PageNumberPagination',],
+    'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 
 }
